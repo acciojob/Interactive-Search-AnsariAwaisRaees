51435da3c7f41762c9cf730c6880ada3e8cdc328
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-02 00:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const searchCom = document.getElementById('searchCom');
+	const searchIn = document.getElementById('searchIn');
+	const searchBtn = document.getElementById('searchBtn');
+
+	function Search(){
+		searchCom.classList.add('active');
+		searchIn.focus();
+	}
+
+	searchBtn.addEventListener('click', Search);
+});
